08535eda0ddfd6a53ef524ee9852b0c8a9430822
mdriazuddinriad5 <mdriazuddinriad5@gmail.com>
2023-11-12 14:21:37 +0600

    shared title section added

--- src/pages/Home/Category/Category.jsx
+++ src/pages/Home/Category/Category.jsx
@@ -8,42 +8,51 @@ import slide2 from '../../../assets/home/slide2.jpg'
 import slide3 from '../../../assets/home/slide3.jpg'
 import slide4 from '../../../assets/home/slide4.jpg'
 import slide5 from '../../../assets/home/slide5.jpg'
+import SectionTitle from '../../../components/SectionTitle/SectionTitle';
 
 
 const Category = () => {
     return (
-        <Swiper
-            slidesPerView={4}
-            spaceBetween={30}
-            centeredSlides={true}
-            pagination={{
-                clickable: true,
-            }}
-            modules={[Pagination]}
-            className="mySwiper mb-24"
-        >
-            <SwiperSlide>
-                <img src={slide1} alt="" />
-                <h2 className="text-4xl text-center uppercase text-white -mt-16">Salads</h2>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={slide2} alt="" />
-                <h2 className="text-4xl text-center uppercase text-white -mt-16 ">Soups</h2>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={slide3} alt="" />
-                <h2 className="text-4xl text-center uppercase text-white -mt-16 ">Pizzas</h2>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={slide4} alt="" />
-                <h2 className="text-4xl text-center uppercase text-white -mt-16 ">Desserts</h2>
-            </SwiperSlide>
-            <SwiperSlide>
-                <img src={slide5} alt="" />
-                <h2 className="text-4xl text-center uppercase text-white -mt-16">Salads</h2>
-            </SwiperSlide>
-           
-        </Swiper>
+
+        <section>
+            <SectionTitle
+                subHeading={'From 11:00am to 10:00pm'}
+                heading={'Order Online'}
+            >
+            </SectionTitle>
+            <Swiper
+                slidesPerView={4}
+                spaceBetween={30}
+                centeredSlides={true}
+                pagination={{
+                    clickable: true,
+                }}
+                modules={[Pagination]}
+                className="mySwiper mb-24"
+            >
+                <SwiperSlide>
+                    <img src={slide1} alt="" />
+                    <h2 className="text-4xl text-center uppercase text-white -mt-16">Salads</h2>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slide2} alt="" />
+                    <h2 className="text-4xl text-center uppercase text-white -mt-16 ">Soups</h2>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slide3} alt="" />
+                    <h2 className="text-4xl text-center uppercase text-white -mt-16 ">Pizzas</h2>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slide4} alt="" />
+                    <h2 className="text-4xl text-center uppercase text-white -mt-16 ">Desserts</h2>
+                </SwiperSlide>
+                <SwiperSlide>
+                    <img src={slide5} alt="" />
+                    <h2 className="text-4xl text-center uppercase text-white -mt-16">Salads</h2>
+                </SwiperSlide>
+
+            </Swiper>
+        </section>
     );
 };
 
